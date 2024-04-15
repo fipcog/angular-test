c482ca041937d10f0ab5c02101364a2dbf13d7fc
Artur <looppinooppi@gmail.com>
2024-04-15 15:51:53 +0300

    feat: implement getTodos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { TodolistsDeckComponent } from './components/todolists-deck/todolists-deck.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DirectivesComponent],
+  imports: [RouterOutlet, DirectivesComponent, TodolistsDeckComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/todolists-deck/todolists-deck.component.ts
+++ src/app/components/todolists-deck/todolists-deck.component.ts
@@ -2,6 +2,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+interface Todolist {
+  addedDate: string
+  id: string
+  order: number
+  title: string
+}
+
 @Component({
   selector: 'app-todolists-deck',
   standalone: true,
@@ -10,6 +17,24 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   ],
   templateUrl: './todolists-deck.component.html',
   styleUrl: './todolists-deck.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodolistsDeckComponent { }
+export class TodolistsDeckComponent {
+  todolists: Todolist[] = []
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getTodos()
+  }
+
+  getTodos() {
+    this.http.get<Todolist[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', {
+      withCredentials: true
+    }).subscribe((res) => {
+      this.todolists = [...res]
+      console.log(this.todolists)
+    })
+  }
+
+}
