a4a197d1f4e27085e435645e6d7250077235f3a4
Artur <looppinooppi@gmail.com>
2024-04-16 12:58:11 +0300

    feat: attach new todo form to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DirectivesComponent } from './components/directives/directives.component';
 import { TodolistsDeckComponent } from './components/todolists-deck/todolists-deck.component';
+import { AddTodoFormComponent } from './components/add-todo-form/add-todo-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DirectivesComponent, TodolistsDeckComponent],
+  imports: [
+    RouterOutlet,
+    DirectivesComponent,
+    TodolistsDeckComponent,
+    AddTodoFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
