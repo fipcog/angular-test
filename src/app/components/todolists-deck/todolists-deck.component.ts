0f18b36e2ca69e21c8873e3e3c206a29ea3a2035
Artur <looppinooppi@gmail.com>
2024-04-16 16:25:19 +0300

    feat: todo api

--- src/app/components/todolists-deck/todolists-deck.component.ts
+++ src/app/components/todolists-deck/todolists-deck.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Todolist } from '../../services/todolistApiService/todolistApiInterfaces';
 import { TodolistApiService } from '../../services/todolistApiService/todolistApi.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -15,50 +16,32 @@ import { TodolistApiService } from '../../services/todolistApiService/todolistAp
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodolistsDeckComponent {
-  todolists: Todolist[] = []
+  todolists$!: Observable<Todolist[]>
 
   constructor(
     private todolistApi: TodolistApiService
   ) { }
 
   ngOnInit() {
-    this.getTodos()
-  }
+    this.todolists$ = this.todolistApi.todolists$
 
-  options = {
-    withCredentials: true
+    this.getTodos()
   }
 
   getTodos() {
     this.todolistApi.getTodos()
-      .subscribe({
-        next: (res) => {
-          this.todolists = [...res]
-        },
-      })
   }
 
   createTodo(title: string) {
     this.todolistApi.createTodo(title)
-      .subscribe((res) => {
-        this.todolists = [res.data.item, ...this.todolists]
-      })
   }
 
   updateTodo(todoId: string, title: string) {
     this.todolistApi.updateTodo(todoId, title)
-      .subscribe(() => {
-        this.todolists = this.todolists.map(
-          td => td.id === todoId ? { ...td, title } : td
-        )
-      })
   }
 
   deleteTodo(todoId: string) {
     this.todolistApi.deleteTodo(todoId)
-      .subscribe(() => {
-        this.todolists = this.todolists.filter(td => td.id !== todoId)
-      })
   }
 }
 
