f0091bb450473f783c69e8c566f8b0fb47f0212a
Artur <looppinooppi@gmail.com>
2024-04-19 13:57:30 +0300

    feat: add form group

--- src/app/components/test-form/test-form.component.ts
+++ src/app/components/test-form/test-form.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-test-form',
@@ -12,6 +13,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TestFormComponent {
+  testForm!: FormGroup
 
-
+  ngOnInit() {
+    this.testForm = new FormGroup({
+      name: new FormControl(''),
+      surname: new FormControl(''),
+      nickname: new FormControl(''),
+      address: new FormGroup({
+        city: new FormControl(''),
+        street: new FormControl('')
+      }),
+      additional: new FormGroup({
+        age: new FormControl('18'),
+        birthday: new FormControl('')
+      })
+    })
+  }
 }
