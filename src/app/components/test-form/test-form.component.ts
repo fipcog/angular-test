9a10b2dc9f5adb423e19a1ff59425d8369173380
Artur <looppinooppi@gmail.com>
2024-04-19 17:01:15 +0300

    feat: validation

--- src/app/components/test-form/test-form.component.ts
+++ src/app/components/test-form/test-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-test-form',
@@ -18,27 +18,63 @@ export class TestFormComponent {
 
   ngOnInit() {
     this.testForm = new FormGroup({
-      name: new FormControl(''),
-      surname: new FormControl(''),
-      nickname: new FormControl(''),
+      name: new FormControl('', [
+        Validators.minLength(3),
+        Validators.maxLength(30)
+      ]),
+      surname: new FormControl('', [
+        Validators.minLength(3),
+        Validators.maxLength(30)
+      ]),
+      nickname: new FormControl('', [
+        Validators.minLength(3),
+        Validators.maxLength(30)
+      ]),
+      email: new FormControl('', [
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+        Validators.required
+      ]),
       address: new FormGroup({
-        city: new FormControl(''),
-        street: new FormControl('')
+        city: new FormControl('', Validators.maxLength(70)),
+        street: new FormControl('', Validators.maxLength(70))
       }),
       additional: new FormGroup({
-        age: new FormControl(''),
-        birthday: new FormControl('')
+        age: new FormControl('', [Validators.min(16)]),
+        birthday: new FormControl('', Validators.required)
       })
     })
+  }
 
-    this.testForm.patchValue({
-      address: {
-        age: '18'
-      }
-    })
+  isFieldInvalid(fieldName: string) {
+    const field = this.testForm.get(fieldName)
+    return field?.invalid && (
+      field?.touched || field?.dirty
+    )
+  }
+
+  checkFieldOnError(fieldName: string, validatorName: string) {
+    const field = this.testForm.get(fieldName)
+    return field?.errors?.[validatorName]
   }
 
   submitHandler() {
     console.log(this.testForm)
+  }
+
+  updateDefault() {
+    this.testForm.patchValue({
+      name: 'Artur',
+      surname: 'Snitko',
+      nickname: 'Nick',
+      email: 'artur1992@gmail.com',
+      address: {
+        city: 'Minsk',
+        street: 'Lenina 17'
+      },
+      additional: {
+        age: '18',
+        birthday: '1992-06-08'
+      },
+    })
   }
 }
