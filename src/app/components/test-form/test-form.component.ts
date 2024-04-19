0899d3bc72b9385ba98983f27769b81fd5b3592f
Artur <looppinooppi@gmail.com>
2024-04-19 14:05:50 +0300

    feat: attached reactive forms mod

--- src/app/components/test-form/test-form.component.ts
+++ src/app/components/test-form/test-form.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test-form',
   standalone: true,
   imports: [
     CommonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './test-form.component.html',
   styleUrl: './test-form.component.scss',
