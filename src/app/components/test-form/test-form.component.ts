e27e6479919df31d4bd407990391860550cec7ef
Artur <looppinooppi@gmail.com>
2024-04-20 12:05:14 +0300

    feat: form builder form

--- src/app/components/test-form/test-form.component.ts
+++ src/app/components/test-form/test-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-test-form',
@@ -15,6 +15,13 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class TestFormComponent {
   testForm!: FormGroup
+  builderForm = this.fb.group({
+    login: ['', Validators.required],
+    password: ['', Validators.required],
+    remember: [false]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.testForm = new FormGroup({
