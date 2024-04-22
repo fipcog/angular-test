dd46ee1f6650f926cfad8d011ddf03d2f7a45f4b
Artur <looppinooppi@gmail.com>
2024-04-22 11:44:10 +0300

    feat: inteceptor error

--- src/app/interceptors/credentials.interceptor.ts
+++ src/app/interceptors/credentials.interceptor.ts
@@ -1,8 +1,18 @@
-import type { HttpInterceptorFn } from '@angular/common/http';
+import type { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
   req = req.clone({
     withCredentials: true,
   })
-  return next(req);
+  return next(req).pipe(
+    catchError((e: HttpErrorResponse) => {
+      let errorMessage: string = ''
+      if (e.status === 500) {
+        errorMessage = 'Error 500'
+      }
+      //.....
+      return throwError(() => new Error(errorMessage))
+    })
+  );
 };
